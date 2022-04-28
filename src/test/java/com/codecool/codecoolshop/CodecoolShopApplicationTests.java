4d75a0cdeef5785d27a51992c1092553a8ed2c24
suaemp <wiktoria.sladowska@gmail.com>
2022-04-28 19:09:53 +0200

    project setup

---
+++ src/test/java/com/codecool/codecoolshop/CodecoolShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.codecoolshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecoolShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
