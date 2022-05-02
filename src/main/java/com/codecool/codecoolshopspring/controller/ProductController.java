1ce7841f315839df824cc481162592a41def84e3
wikuuu1 <wiktoria.rajba391@gmail.com>
2022-05-02 20:13:05 +0200

    IMPLEMENTED: displaying products in tablets category

--- src/main/java/com/codecool/codecoolshopspring/controller/ProductController.java
+++ src/main/java/com/codecool/codecoolshopspring/controller/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class ProductController {
@@ -18,11 +19,20 @@ public class ProductController {
 
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("title", "Codecool Shop");
         model.addAttribute("category", service.getAllProductCategories());
-        System.out.println(service.getAllProductCategories());
-//        model.addAttribute("products", service.getProductsForCategory(1));
         model.addAttribute("products", service.getAllProducts());
         System.out.println(service.getAllProducts());
         return "product/index";
+    }
+
+    @GetMapping("/Tablet")
+    public String displayTabletProducts(Model model){
+        model.addAttribute("title", "Tablet category");
+        model.addAttribute("choosenCategory", service.getProductCategory(1));
+        model.addAttribute("allCategories", service.getAllProductCategories());
+        model.addAttribute("products", service.getProductsForCategory(1));
+
+        return "product/tablets";
     }
 }
